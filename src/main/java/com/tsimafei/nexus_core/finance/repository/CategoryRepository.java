afa9b8abb59d57bb822137260ee874ada7695b88
tsimafei-yu <tsimafeiyutskevich@gmail.com>
2026-08-13 11:27:53 +0200

    add CategoryRepository

---
+++ src/main/java/com/tsimafei/nexus_core/finance/repository/CategoryRepository.java
@@ -0,0 +1,12 @@
+package com.tsimafei.nexus_core.finance.repository;
+
+import com.tsimafei.nexus_core.finance.domain.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+    Optional<Category> findByNameIgnoreCase(String name);
+}
