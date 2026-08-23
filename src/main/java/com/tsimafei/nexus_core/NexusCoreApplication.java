7e7de8bc9418ae11554a7711a397d817d633ec06
tsimafei-yu <tsimafeiyutskevich@gmail.com>
2026-08-23 17:42:50 +0200

    fix timezone

--- src/main/java/com/tsimafei/nexus_core/NexusCoreApplication.java
+++ src/main/java/com/tsimafei/nexus_core/NexusCoreApplication.java
@@ -1,12 +1,21 @@
 package com.tsimafei.nexus_core;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 @EnableScheduling
 public class NexusCoreApplication {
+
+	@PostConstruct
+	public void init() {
+		// Set default timezone for JVM
+		TimeZone.setDefault(TimeZone.getTimeZone("Europe/Warsaw"));
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(NexusCoreApplication.class, args);
