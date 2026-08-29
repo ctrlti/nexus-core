fa94c470db923185fc8a5f663bc4bd6f4b855837
yeap <tsimafeiyutskevich@gmail.com>
2026-08-29 21:36:57 +0200

    test context

--- src/test/java/com/tsimafei/nexus_core/NexusCoreApplicationTests.java
+++ src/test/java/com/tsimafei/nexus_core/NexusCoreApplicationTests.java
@@ -2,12 +2,17 @@ package com.tsimafei.nexus_core;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.telegram.telegrambots.longpolling.TelegramBotsLongPollingApplication;
 
 @SpringBootTest
 class NexusCoreApplicationTests {
 
+	@MockitoBean
+	private TelegramBotsLongPollingApplication telegramBotsLongPollingApplication;
+
 	@Test
 	void contextLoads() {
+		// test
 	}
-
 }
